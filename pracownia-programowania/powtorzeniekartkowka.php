b8852cc590232d4149443f8211e3be158e34bef7
Konrad <konradmalina@wp.pl>
2021-10-13 04:23:55 +0200

    2

--- pracownia-programowania/powtorzeniekartkowka.php
+++ pracownia-programowania/powtorzeniekartkowka.php
@@ -1,15 +1,50 @@
 <!DOCTYPE html>
-<html>
- <head>
-   <meta charset="UTF-8">
- </head>
- <body>
-   <h3>Dane użytkownika<h3>
-   <form action="5_1_data.php" method="get">
-       <input type="text" name="name" placeholder="Imię" /><br><br>
-       <input type="text" name="nationallity" placeholder="Narodowość" /><br><br>
-       <input type="number" name="age" placeholder="Wiek" /><br><br>
-       <input type="submit" value="Zatwierdź" /><br><br>
-   </form>
- </body>
+<html lang="PL-pl">
+<head>
+  <meta charset="utf-8">
+</head>
+<body>
+  <?php
+    if (!isset($_POST['person']) && !isset($_POST['buttonAvg']))
+    {
+      echo <<< FORMCOUNTPERSON
+      <h3>Ilość osób w rodzinie</h3>
+      <form method="post">
+      <input type="number" name="person" placeholder="Podaj ilość osób"><br><br>
+      <input type="submit" value="Zatwierdź">
+      </form>
+      FORMCOUNTPERSON;
+    }
+  ?>
+  <?php
+    if (!empty($_POST['person'])) {
+      echo "<h3>Ilość osób w rodzinie: $_POST[person]</h3>";
+      echo <<< FORMAGEPERSON
+      <form method="post">
+      FORMAGEPERSON;
+      $count = $_POST['person'];
+      for ($i=1; $i <= $count; $i++) {
+        echo "<input type='number' name='person$i' placeholder='Podaj wiek osoby nr $i'><br><br>";
+      }
+      echo <<< FORMAGEPERSON
+      <input type="submit" name="buttonAvg" value="Oblicz">
+      </form>
+      FORMAGEPERSON;
+    }
+
+    if (isset($_POST['buttonAvg'])) {
+      $avg = 0;
+      $count = 0;
+      foreach ($_POST as $key => $value) {
+        //echo '$key: $value<br>'
+        if($key != 'buttonAvg')
+        {
+          $avg += $value;
+          $count++;
+        }
+      }
+      echo "Średni wiek ".numer_format($avg/$count, 2, " ")."<br>";
+    }
+   ?>
+</body>
 </html>
